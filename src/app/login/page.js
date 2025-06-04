113f559f9ccc90d230e3a36f09383f3656009a43
kuldeep saini <kuldeepsaini5514@gmail.com>
2025-06-04 09:43:04 +0530

    Login Page Bug Fix

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -8,7 +8,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import styles from "@/utils/styles";
 
-const login = () => {
+const Login = () => {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -144,4 +144,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Login;
